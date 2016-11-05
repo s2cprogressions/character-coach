b383d335eb64a7fea26b1d17b01f142bc7a2bc62
Drew Colthorp <colthorp@atomicobject.com>
2016-11-04 23:06:28 -0400

    Spike model loading/merging

--- app/services/content_model.rb
+++ app/services/content_model.rb
@@ -1,5 +1,17 @@
-class ContentModel
+CATEGORY_KEYS = %w(
+  title
+  challenges
+  thinking
+  feelings
+  concerns
+  successful_thinking
+  successful_feelings
+).freeze
 
+# TODO: Make me a real boy
+Category = Struct.new(*CATEGORY_KEYS.map(&:to_sym))
+
+class ContentModel
   def self.instance
     @instance ||=
       begin
@@ -13,12 +25,37 @@ class ContentModel
     @content = normalize(content)
   end
 
-  def normailze(content)
-    # categories = content['categories']
-    # globals = content['globals']
-    # categories.each_pair do |cat|
-    #   globals.each_pair do |type_key, type_globals|
-    #   end
-    # end
+  attr_reader :content
+
+  private def normalize(content)
+    categories = content["categories"]
+    globals = content["globals"]
+
+    categories.each_pair do |_cat_key, cat|
+      globals.each_pair do |type_key, type_globals|
+        cat_entry = cat[type_key]
+        if cat_entry.blank?
+          cat_entry = []
+          cat[type_key] = cat_entry
+        end
+        cat_entry.concat type_globals
+      end
+    end
+
+    categories.freeze
+  end
+
+  def category_keys
+    @content.keys
+  end
+
+  def categories
+    category_keys.map(&method(:category_by_key))
+  end
+
+  def category_by_key(key)
+    entry = @content[key.to_s]
+    args = entry.values_at(*CATEGORY_KEYS)
+    Category.new(*args)
   end
 end
