a1a757c98a5d906db4986a7e9d61b53a14546eb5
Drew Colthorp <colthorp@atomicobject.com>
2016-11-04 20:58:37 -0400

    bootstrap and home controller

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Progressions
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.app_generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+      g.performance_tool :rspec
+      g.javascript_engine :js
+    end
   end
 end
