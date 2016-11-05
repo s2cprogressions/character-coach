88ba784019cdbfc06378ddb66c7125c6b08ed21e
Joshua Kovach <kovach.jc@gmail.com>
2016-11-05 08:49:01 -0400

    Add Activity model parsed from model.yml

---
+++ app/models/activity.rb
@@ -0,0 +1,34 @@
+class Activity
+  include ActiveModel::Model
+
+  attr_accessor :id, :title, :challenges, :thinking, :feelings, :concerns,
+                :successful_thinking, :successful_behaviors
+
+  def self.repository
+    YAML.load_file(Rails.root.join("config", "model.yml"))
+  end
+
+  def self.globals
+    repository["globals"]
+  end
+
+  def self.categories
+    repository["categories"].map do |id, category|
+      globals.each do |key, value|
+        category["id"] = id
+        category[key] += value
+      end
+      category
+    end
+  end
+
+  def self.activities
+    categories.map do |category|
+      new(category)
+    end
+  end
+
+  def self.activity(id)
+    activities.index_by(&:id)[id.to_s]
+  end
+end
---
+++ spec/models/activity_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe Activity, type: :model do
+  describe ".activities" do
+    subject(:activities) { Activity.activities }
+    it "returns an array of Activity instances" do
+      expect(activities).to all(be_a(Activity))
+    end
+
+    it "sets the titles" do
+      expect(activities.map(&:title)).to contain_exactly "School", "Trouble", "Attitude"
+    end
+
+    it "sets the ids" do
+      expect(activities.map(&:id)).to contain_exactly "school", "trouble", "attitude"
+    end
+
+    it "sets the feelings" do
+      global_feelings = %w(Angry Worried Anxious Nervous Sad Scared Mad Okay Content)
+      expect(activities.map(&:feelings)).to contain_exactly global_feelings, global_feelings, global_feelings
+    end
+  end
+
+  describe ".activity" do
+    it "returns an Activity" do
+      school = Activity.activity(:school)
+      expect(school).to be_a Activity
+    end
+
+    it "returns the activity for the given id" do
+      school = Activity.activity(:school)
+      expect(school.id).to eq "school"
+    end
+  end
+end
