515092dbf533bf6b97ac802fcfbf3ec361f3923a
Joshua Kovach <kovach.jc@gmail.com>
2016-11-05 12:55:21 -0400

    Click a specific activity challenge

--- spec/features/activities/full_play_spec.rbs_spec.rb
+++ spec/features/activities/full_play_spec.rbs_spec.rb
@@ -7,5 +7,11 @@ RSpec.feature "Playing through an activity", :js, type: :feature do
     Activity.activities.flat_map(&:challenges).each do |challenge|
       expect(page).to have_content challenge
     end
+
+    activity = Activity.activity(:school)
+
+    within "#activity_#{activity.id}_challenge_0" do
+      click_link "Start"
+    end
   end
 end
