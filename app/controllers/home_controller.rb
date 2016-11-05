7db347f9d25e2adee1eb3952acaa55811448b964
Drew Colthorp <colthorp@atomicobject.com>
2016-11-04 22:31:57 -0400

    pages defined with blank templates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,43 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def challenges
+  end
+
+  def not_alone
+  end
+
+  def thoughts
+  end
+
+  def feelings
+  end
+
+  def concerns
+  end
+
+  def thanks
+  end
+
+  def summary
+  end
+
+  def let_me_help
+  end
+
+  def positive_thoughts
+  end
+
+  def positive_emotions
+  end
+
+  def positive_behaviors
+  end
+
+  def success_plan
+  end
+
+  def complete
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,19 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'home#index'
+
+  get 'challenges', to: 'home#challenges'
+  get 'complete', to: 'home#complete'
+  get 'concerns', to: 'home#concerns'
+  get 'feelings', to: 'home#feelings'
+  get 'let_me_help', to: 'home#let_me_help'
+  get 'not_alone', to: 'home#not_alone'
+  get 'positive_behaviors', to: 'home#positive_behaviors'
+  get 'positive_emotions', to: 'home#positive_emotions'
+  get 'positive_thoughts', to: 'home#positive_thoughts'
+  get 'success_plan', to: 'home#success_plan'
+  get 'summary', to: 'home#summary'
+  get 'thanks', to: 'home#thanks'
+  get 'thoughts', to: 'home#thoughts'
+
 end
