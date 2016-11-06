b9edc09ec3628b8d1ef1977fb7d52946c9f838c2
Drew Colthorp <colthorp@atomicobject.com>
2016-11-06 13:25:17 -0500

    more styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     options.reverse_merge!(autolink: true, tables: true)
     Redcarpet::Markdown.new(Redcarpet::Render::HTML, options).render(text).html_safe
   end
+
+  def next_button(text, path)
+    link_to text, path,
+              class: "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent next-button"
+  end
 end
