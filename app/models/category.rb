e1069bb994fd32d44e1e7aab677e786c8fdb8f76
Joshua Kovach <kovach.jc@gmail.com>
2016-11-05 23:46:23 -0400

    Dependent destroy category options

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,10 @@
 class Category < ApplicationRecord
-  has_many :challenges, -> { where(type: "challenge").order(index: :asc) }, class_name: "CategoryOption"
-  has_many :feelings, -> { where(type: "feeling").order(index: :asc) }, class_name: "CategoryOption"
-  has_many :thoughts, -> { where(type: "thought").order(index: :asc) }, class_name: "CategoryOption"
-  has_many :concerns, -> { where(type: "concern").order(index: :asc) }, class_name: "CategoryOption"
-  has_many :positive_thoughts, -> { where(type: "positive_thought").order(index: :asc) }, class_name: "CategoryOption"
-  has_many :positive_behaviors, -> { where(type: "positive_behavior").order(index: :asc) }, class_name: "CategoryOption"
+  has_many :challenges, -> { where(type: "challenge").order(index: :asc) }, class_name: "CategoryOption", dependent: :destroy
+  has_many :feelings, -> { where(type: "feeling").order(index: :asc) }, class_name: "CategoryOption", dependent: :destroy
+  has_many :thoughts, -> { where(type: "thought").order(index: :asc) }, class_name: "CategoryOption", dependent: :destroy
+  has_many :concerns, -> { where(type: "concern").order(index: :asc) }, class_name: "CategoryOption", dependent: :destroy
+  has_many :positive_thoughts, -> { where(type: "positive_thought").order(index: :asc) }, class_name: "CategoryOption", dependent: :destroy
+  has_many :positive_behaviors, -> { where(type: "positive_behavior").order(index: :asc) }, class_name: "CategoryOption", dependent: :destroy
 
   accepts_nested_attributes_for :challenges, :feelings, :thoughts, :concerns,
                                 :positive_thoughts, :positive_behaviors,
