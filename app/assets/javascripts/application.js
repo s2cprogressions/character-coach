1bcdf687fecc92ada38c74f4ba18d5bde69cf1bd
Drew Colthorp <colthorp@atomicobject.com>
2016-11-06 08:40:00 -0500

    Animated links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,3 +36,20 @@ function getNavigationBarColor() {
 function hideNavigationBarForMobile() {
   // TODO: implement?
 }
+
+$('#activitiesSubmit').click(function (event) {
+  alert('Hooray!');
+});
+
+$.fn.extend({
+  animateCss: function (animationName) {
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    this.addClass('animated ' + animationName).one(animationEnd, function() {
+      $(this).removeClass('animated ' + animationName);
+    });
+  }
+});
+
+function addAnimateCSS() {
+  $('#activities').animateCss('bounce');
+}
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,18 +1,28 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$('#activitiesSubmit').click(function (event) {
-    alert('Hooray!');
+
+
+
+document.addEventListener("turbolinks:load", function() {
+  $(document.body).on('click', '.animated-link', function (event) {
+    event.preventDefault();
+    var $this = $(this);
+    $this.animateCss('bounce')
+    setTimeout(function() {
+      Turbolinks.visit($this.attr('href'));
+    }, 1000)
+  });
 });
 
 $.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
+  animateCss: function (animationName) {
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    this.addClass('animated ' + animationName).one(animationEnd, function() {
+      $(this).removeClass('animated ' + animationName);
+    });
+  }
 });
 
 function addAnimateCSS() {
-    $('#activities').animateCss('bounce');
+  $('#activities').animateCss('bounce');
 }
