550164a0e2a56a53fafaed19dc0a526e0516d213
Drew Colthorp <colthorp@atomicobject.com>
2016-11-05 20:02:40 -0400

    Strip emoji prefix from titles

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ApplicationRecord
-
-
+  EMOJI_REGEX = /^:\w+:/
   %w(
     challenges
     thinking
@@ -10,11 +9,14 @@ class Category < ApplicationRecord
     positive_behaviors
   ).each do |col|
     define_method :"#{col}_titles" do
-      __send__(col).split("\n") #.map{ |line| line[/(?<=:\w+).*/] }
+      __send__(col).split("\n").map{ |line|
+        emoji = line[EMOJI_REGEX]
+        line.slice(emoji.size..-1).strip
+      }
     end
 
     define_method :"#{col}_emojis" do
-      __send__(col).split("\n").map{ |line| line[/^:\w+:/] }
+      __send__(col).split("\n").map{ |line| line[EMOJI_REGEX] }
     end
   end
 end
